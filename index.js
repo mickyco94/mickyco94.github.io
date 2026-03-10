0fd021b5f2876070ab9b6fa21462b7f2762e48d0
mickyco94 <mickyclerkinoliver@gmail.com>
2026-03-10 21:52:59 +0000

    messing about with helix and rotations

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-FG = "green";
-BG = "gray";
+FG = "#7d4e57";
+FG = "#b166cc";
+BG = "#11151c";
 
 const canvas = document.getElementById("game");
-canvas.width = 400;
-canvas.height = 400;
+canvas.width = 950;
+canvas.height = 950;
 const ctx = canvas.getContext("2d");
 
 function clear() {
@@ -49,15 +50,46 @@ function translate_z({ x, y, z }, dz) {
   }
 }
 
-// x' = xcos(theta) - ysin(theta) 
-// y' = xsin(theta) + ycos(theta)
-function rotate_xz({ x, y, z }, angle) {
+function translate_y({ x, y, z }, dy) {
+  return {
+    x, y: y + dy, z,
+  }
+}
+
+function translate_x({ x, y, z }, dx) {
+  return { x: x + dx, y, z };
+}
+
+function scale({ x, y, z }, factor) {
+  return {
+    x: x * factor, y: y * factor, z: z * factor,
+  }
+}
+
+function scale_y({ x, y, z }, factor) {
+  return { x, y: y * factor, z };
+}
+
+function scale_z({ x, y, z }, factor) {
+  return { x, y, z: z * factor };
+}
+
+function scale_x({ x, y, z }, factor) {
+  return { x: x * factor, y, z };
+}
+
+
+function rotate_xz({ x, y, z }, angle, origin = { x: 0, y: 0, z: 0 }) {
   const c_theta = Math.cos(angle);
   const s_theta = Math.sin(angle);
+  const p = {
+    x: (x - origin.x),
+    z: (z - origin.z),
+  }
   return {
-    x: x * c_theta - z * s_theta,
+    x: origin.x + (p.x * c_theta - p.z * s_theta),
     y,
-    z: x * s_theta + z * c_theta,
+    z: origin.z + (p.x * s_theta + p.z * c_theta),
   }
 }
 
@@ -71,19 +103,30 @@ function rotate_xy({ y, x, z }, angle) {
   }
 }
 
-const vs = [
-  { x: 0.25, y: 0.25, z: 0.25 },
-  { x: -0.25, y: 0.25, z: 0.25 },
-  { x: -0.25, y: -0.25, z: 0.25 },
-  { x: 0.25, y: -0.25, z: 0.25 },
+function helix(time) {
+  res = []
+  const radius = (0.1 * Math.sin(time)) + 0.2;
+  const pitch = .3;
+  for (let t = -10; t <= 10; t += 0.2) {
+    res.push({ x: radius * Math.cos(t), y: (pitch * t) / (2 * Math.PI), z: radius * Math.sin(t) })
+    res.push({ x: -radius * Math.cos(t), y: (pitch * t) / (2 * Math.PI), z: -radius * Math.sin(t) })
+  }
+  return res;
+}
 
-  { x: 0.25, y: 0.25, z: -0.25 },
-  { x: -0.25, y: 0.25, z: -0.25 },
-  { x: -0.25, y: -0.25, z: -0.25 },
-  { x: 0.25, y: -0.25, z: -0.25 },
+const cube_vs = [
+  { x: 1, y: 1, z: 1 },
+  { x: -1, y: 1, z: 1 },
+  { x: -1, y: -1, z: 1 },
+  { x: 1, y: -1, z: 1 },
+
+  { x: 1, y: 1, z: -1 },
+  { x: -1, y: 1, z: -1 },
+  { x: -1, y: -1, z: -1 },
+  { x: 1, y: -1, z: -1 },
 ]
 
-const fs = [
+const cube_fs = [
   [0, 1, 2, 3],
   [4, 5, 6, 7],
   [0, 4],
@@ -97,18 +140,51 @@ const dt = 1 / FPS;
 let angle = 0;
 let time = 0;
 
-function frame() {
-  clear();
+const rand = () => Math.random() * 2 - 1;
+
+const random_points = [...Array(500)].map(() => ({
+  x: rand(),
+  y: rand(),
+  z: rand(),
+}));
+
+
+
+function draw_cube() {
+  const speed = 0.5;
   time += dt;
-  angle = Math.PI * Math.sin(time);
-  const transform = (p) => screen(project(translate_z(rotate_xz(p, angle), 1)));
-  for (const f of fs) {
+  angle = (Math.PI / 2) * Math.cos(speed * time);
+
+  const transform = (v) => {
+    const rotated = rotate_xy(rotate_xz(v, Math.PI / 2 * speed * time), angle);
+    const scaled = scale(rotated, 0.3);
+    const translated = translate_z(scaled, 1);
+    return screen(project(translated));
+  }
+  for (const f of cube_fs) {
     for (let i = 0; i < f.length; i++) {
-      const a = vs[f[i]];
-      const b = vs[f[(i + 1) % f.length]];
+      const a = cube_vs[f[i]];
+      const b = cube_vs[f[(i + 1) % f.length]];
       line(transform(a), transform(b));
     }
   }
+  random_points.map(v => {
+    const oscillateTowardsCenter = (v, time, f = 0.2) => ({
+      x: v.x * (f + (1 - f) * (1 + Math.cos(time)) / 2),
+      y: v.y * (f + (1 - f) * (1 + Math.cos(time + Math.PI / 6)) / 2),
+      z: v.z * (f + (1 - f) * (1 + Math.cos(time + Math.PI / 3)) / 2)
+    });
+    const oscillate = oscillateTowardsCenter(v, time);
+    const rotated = rotate_xy(rotate_xz(oscillate, Math.PI / 2 * speed * time), angle);
+    const scaled = scale(rotated, 0.3);
+    const translated = translate_z(scaled, 1);
+    return point(screen(project(translated)));
+  });
+}
+
+function frame() {
+  clear();
+  draw_cube();
   setTimeout(frame, 1000 / FPS);
 }
 
